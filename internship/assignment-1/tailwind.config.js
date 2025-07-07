03f950e61b7b2dfb7b0f5f7d326dde98858b5a64
Mustafa <mustafa25008c@gmail.com>
2025-07-07 12:10:46 +0500

    Update tailwind.config.js

--- internship/assignment-1/tailwind.config.js
+++ internship/assignment-1/tailwind.config.js
@@ -1,13 +1,5 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",       // for pages like page.tsx, layout.tsx
-    "./src/components/**/*.{js,ts,jsx,tsx}", // for your UI components
-    "./src/data/**/*.{js,ts,jsx,tsx}",       // optional if you're using dynamic classes in data
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+content: [
+  "./src/app/**/*.{js,ts,jsx,tsx}",
+  "./src/components/**/*.{js,ts,jsx,tsx}",
+  "./src/data/**/*.{js,ts,jsx,tsx}",
+]
